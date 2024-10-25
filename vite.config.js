89d1a1ae5f6e7fc9cb503c14ae93d897d91f9746
Mudit jain <muditj214@gmail.com>
2024-10-25 12:03:12 +0530

    base added to vite.config for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://muditjainn.github.io/color-changing-react-vite/',
   plugins: [react()],
 })
